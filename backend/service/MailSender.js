4444f81eccfd07228f5c14d2f36d9afb289a9834
Lars Ratovo <lars.ratovo@gmail.com>
2024-03-26 21:56:14 +0300

    Update MailSender.js

--- backend/service/MailSender.js
+++ backend/service/MailSender.js
@@ -6,8 +6,8 @@ const transporter = nodemailer.createTransport({
   port: 587,
   secure: false,
   auth: {
-    user: "my.randrianantoandro@gmail.com",
-    pass: "slvaguwmbnxhlfzl",
+    user: "xxxxxx",
+    pass: "xxxx",
   },
 });
 
